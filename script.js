2f0f8eb98c04ccff6df21d77f99719bfa40a22cf
Saras9 <99237535+Saras9@users.noreply.github.com>
2022-03-22 19:27:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var str="Skill Safari" 
+console.log("length of the string is "+str.length)
+console.log(str.toUpperCase(str))
+console.log("index position of \"a\" "+str.indexOf("a"))
+console.log(" CapAmerica: fubyabc \n Iron Man: hello,jbcklbuelk jncbuia \n CapAmerica: mcds uig fyebf \n CapAmerica:bye")
+var height=1.6
+var weight=50
+var bmi=weight/(height^2)
+console.log("BMI is "+Math.round(bmi))
